1194794eb49f822edd9870a732810313fcc314aa
jldfy <jaouad.lahlou.dfy@gmail.com>
2025-02-12 00:18:27 +0100

    add cv

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Portfolio } from './components/Portfolio';
+import { Portfolio } from './components/Portfolio.1';
 import { CaseStudy } from './components/CaseStudy';
 import { ArtPortfolio } from './components/ArtPortfolio';
 
