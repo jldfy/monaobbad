98660583d5ea7057503a165efeabe58653ece3cf
jldfy <jaouad.lahlou.dfy@gmail.com>
2025-02-24 16:00:26 +0100

    commit chemin

--- src/components/DesignProcess.tsx
+++ src/components/DesignProcess.tsx
@@ -14,7 +14,7 @@ export function DesignProcess() {
           <div className="relative max-w-3xl mx-auto">
       <div className="p-8 bg-gray-50 border-2 border-gray-300 border-dashed rounded-xl">
         <img
-        src="./src/data/prototype.PNG"
+        src="https://github.com/jldfy/monaobbad/commit/3bec04b287b5c87ac98a74e09b6dbfea58600222#diff-316b6398aaf74fbdfc5ea0d96f95016fa710345c2e0356c267b190a4568586ab"
         alt="wireframe"
         />
         </div>
